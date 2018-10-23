6cc687c4c623a8cc739cefa715f8624dda64f316
Loginless <Loginless@bigmir.net>
2018-10-24 00:33:23 +0300

    My_HW3_Added "final" in MealTestData for test data

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -13,17 +13,17 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 public class MealTestData {
 
-    public static int USER_MEAL_ID = 100007;
-    public static int ADMIN_MEAL_ID = 100009;
+    public static final int USER_MEAL_ID = 100007;
+    public static final int ADMIN_MEAL_ID = 100009;
 
-    public static Meal MEAL1 = new Meal(100002, LocalDateTime.of(2018, 5, 23, 10, 00), "Potato", 1300);
-    public static Meal MEAL2 = new Meal(100003, LocalDateTime.of(2018, 5, 23, 13, 00), "Fish and Mushrooms", 1200);
-    public static Meal MEAL3 = new Meal(100004, LocalDateTime.of(2018, 5, 23, 20, 00), "Soup", 400);
-    public static Meal MEAL4 = new Meal(100005, LocalDateTime.of(2018, 6, 5, 10, 00), "Beer and Chips", 5000);
-    public static Meal MEAL5 = new Meal(100006, LocalDateTime.of(2018, 6, 5, 15, 00), "Sweets", 1000);
-    public static Meal USER_TEST_MEAL = new Meal(USER_MEAL_ID, LocalDateTime.of(2018, 6, 5, 21, 00), "Biscuits", 250);
-    public static Meal MEAL7 = new Meal(100008, LocalDateTime.of(2018, 10, 23, 8, 00), "Breakfast", 2500);
-    public static Meal ADMIN_TEST_MEAL = new Meal(ADMIN_MEAL_ID, LocalDateTime.of(2018, 10, 23, 20, 00), "Dinner", 3500);
+    public static final Meal MEAL1 = new Meal(100002, LocalDateTime.of(2018, 5, 23, 10, 00), "Potato", 1300);
+    public static final Meal MEAL2 = new Meal(100003, LocalDateTime.of(2018, 5, 23, 13, 00), "Fish and Mushrooms", 1200);
+    public static final Meal MEAL3 = new Meal(100004, LocalDateTime.of(2018, 5, 23, 20, 00), "Soup", 400);
+    public static final Meal MEAL4 = new Meal(100005, LocalDateTime.of(2018, 6, 5, 10, 00), "Beer and Chips", 5000);
+    public static final Meal MEAL5 = new Meal(100006, LocalDateTime.of(2018, 6, 5, 15, 00), "Sweets", 1000);
+    public static final Meal USER_TEST_MEAL = new Meal(USER_MEAL_ID, LocalDateTime.of(2018, 6, 5, 21, 00), "Biscuits", 250);
+    public static final Meal MEAL7 = new Meal(100008, LocalDateTime.of(2018, 10, 23, 8, 00), "Breakfast", 2500);
+    public static final Meal ADMIN_TEST_MEAL = new Meal(ADMIN_MEAL_ID, LocalDateTime.of(2018, 10, 23, 20, 00), "Dinner", 3500);
 
     public static void assertMatch(Meal actual, Meal expected) {
         assertThat(actual).isEqualToIgnoringGivenFields(expected, "id");
