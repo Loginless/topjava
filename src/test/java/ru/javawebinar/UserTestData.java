ecc164a1c6273b4d6be4781ddc71bdd0459b58ad
Loginless <Loginless@bigmir.net>
2018-11-20 01:50:23 +0200

    Optional

--- src/test/java/ru/javawebinar/UserTestData.java
+++ src/test/java/ru/javawebinar/UserTestData.java
@@ -1,9 +1,10 @@
-package ru.javawebinar.topjava;
+package ru.javawebinar;
 
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
@@ -13,7 +14,7 @@ public class UserTestData {
     public static final int ADMIN_ID = START_SEQ + 1;
 
     public static final User USER = new User(USER_ID, "User", "user@yandex.ru", "password", Role.ROLE_USER);
-    public static final User ADMIN = new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.ROLE_ADMIN);
+    public static final User ADMIN = new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.ROLE_ADMIN, Role.ROLE_USER);
 
     public static void assertMatch(User actual, User expected) {
         assertThat(actual).isEqualToIgnoringGivenFields(expected, "registered", "roles", "meals");
@@ -26,4 +27,8 @@ public class UserTestData {
     public static void assertMatch(Iterable<User> actual, Iterable<User> expected) {
         assertThat(actual).usingElementComparatorIgnoringFields("registered", "roles", "meals").isEqualTo(expected);
     }
-}
+
+    public static void assertMatch(Set<Role> actual, Set<Role> expected) {
+        assertThat(actual).isEqualTo(expected);
+    }
+    }
